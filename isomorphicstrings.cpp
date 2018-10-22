189fd13965be02b7a44aa26f32905a36068a23ee
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-10-21 22:04:31 -0500

    Isomorphic strings CodeJam

---
+++ isomorphicstrings.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<map>
+#include<string>
+
+using namespace std;
+bool isIsomorphic( string s, string t) {
+
+    typedef map<string,int>::iterator m_iter;
+ 	m_iter itrs;	
+ 	m_iter itrt;	
+	std::map<char, int> map_s;
+	std::map<char, int> map_t;
+
+	for (int i = 0; i < s.size(); i++) {
+		
+
+	}
+}
+
+                
+                itr = m.find(p.first);
+                if(itr != m.end()){m[p.first] += p.second;} else {
+                m.insert(p); }
+                break;
+            case 2: 
+                cin >> h;
+                itr = m.find(h);
+                if(itr != m.end()) {m[h] = 0 ;}
+                break;
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
