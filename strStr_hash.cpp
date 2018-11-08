0de662f55b9597fe3caedab5d5709665a71460eb
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-11-07 23:08:40 -0600

    String substring CodeJam

--- strStr_hash.cpp
+++ strStr_hash.cpp
@@ -6,31 +6,31 @@
 
 using namespace std;
 
-int strStr(string haystack, string needle){
+int strStr(string haystack, string needle) {
 
 	int i = 0;
 	int n = haystack.size();
 	int m = needle.size();
-	if(needle.size() == 0) { return 0;}
+	if (needle.size() == 0) { return 0;}
 
 	std::set<string> mySet;
 	mySet.insert(needle);
 //	string::iterator itr1=haystack.begin();
 	//string::iterator itr2=itr1 + needle.size();
-	
+
 	for (i = 0; i < n - m; i++) {
-		string temp = haystack.substr(i,m);
+		string temp = haystack.substr(i, m);
 
 		if (mySet.find(temp) != mySet.end()) {
 			mySet.insert(temp);
-			} else return i;
-		
+		} else return i;
+
 	}
 	return -1;
 
 }
 int main(int argc, const char *argv[])
 {
-	
+
 	return 0;
 }
