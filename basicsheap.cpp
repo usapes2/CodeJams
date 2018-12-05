4f3ca0752b7e264c3b8ebeff8f04b6deec1f75d9
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-12-05 15:58:36 -0600

    Basics about heap in cpp

---
+++ basicsheap.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<map>
+#include<set>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(int argc, const char *argv[])
+{
+	vector<int> v = {
+	1,2,3,4,5,5,5,5};
+		std::map<int, int> m(begin(v),end(v)) ;
+		for (auto i : container) {
+			
+		}
+	return 0;
+}
