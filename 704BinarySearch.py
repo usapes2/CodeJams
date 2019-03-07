0facad6234afd5d0e4a8bad9d3aa8915e6282bcf
Uladzimir Sapeshka <usapeshka@gmail.com>
2019-03-07 13:46:00 -0600

    Binary Search python CodeJam

---
+++ 704BinarySearch.py
@@ -0,0 +1,13 @@
+def search(nums,target):
+    l , r = 0 , len(nums) - 1
+    while(l <= r):
+        mid = (l+r)//2
+        if (nums[mid] == target):
+            return mid
+        elif (nums[mid] < target):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
+print(search([0,2,3,7,10,100,200,1000], 1000))
